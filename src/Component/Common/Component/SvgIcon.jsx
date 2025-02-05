4798fd073400c13fa330f4fe2e2ec440a58c3213
Misba Grafizen <misba@grafizen.com>
2025-02-06 01:45:30 +0530

    updated icons

--- src/Component/Common/Component/SvgIcon.jsx
+++ src/Component/Common/Component/SvgIcon.jsx
@@ -1,15 +1,31 @@
+// import React from 'react';
+
+// const SvgIcon = (props) => {
+//   const { iconId, ...res } = props;
+//   return (
+
+//     <>
+//           <svg {...res}>
+//       {/* <use href={`/svg/sprite.svg#${iconId}`}></use> */}
+//       <use href={`../../ #${iconId}`}></use>
+//     </svg>
+//     </>
+//   );
+// };
+
+// export default SvgIcon;
+
+
 import React from 'react';
 
 const SvgIcon = (props) => {
   const { iconId, ...res } = props;
   return (
-
-    <>
-          <svg {...res}>
-      <use href={`/svg/sprite.svg#${iconId}`}></use>
+    <svg {...res}>
+      <use href={`${import.meta.env.BASE_URL}svg/sprite.svg#${iconId}`}></use>
     </svg>
-    </>
   );
 };
 
 export default SvgIcon;
+
